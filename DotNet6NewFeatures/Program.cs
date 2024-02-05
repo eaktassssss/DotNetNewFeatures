02ed52dee7a13ba7ba4eb07f0e66cd7a4e3af075
evren <evren@EVRENAKTAS>
2024-02-05 22:18:48 +0300

    JsonSerializer DeserializeAsyncEnumerable

---
+++ DotNet6NewFeatures/Program.cs
@@ -0,0 +1,44 @@
+﻿using DotNet6NewFeatures.Entities;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+static async Task Main(string[] args)
+{
+    using (var httpClient = new HttpClient())
+    {
+
+        #region DeserializeAsync
+        var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+        var content = await response.Content.ReadAsStreamAsync();
+        var datas = await JsonSerializer.DeserializeAsync<IEnumerable<Posts>>(content);
+        foreach (var item in datas)
+        {
+            Console.WriteLine($"id={item.id}-- title={item.title}");
+        }
+        #endregion
+
+
+        #region DeserializeAsyncEnumerable
+        await foreach (var item in JsonSerializer.DeserializeAsyncEnumerable<Posts>(content))
+        {
+            Console.WriteLine($"id={item.id}-- title={item.title}");
+        }
+        #endregion
+    }
+}
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
